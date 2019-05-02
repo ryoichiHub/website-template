4ca5a4c3fe56f3110b57cf56d43de07f180fdd1e
Ryoichi Shiohama <ryoichi.shiohama@gmail.com>
2019-05-02 23:55:47 +0900

    JavaScriptでのレスポンシブ対応サンプルコード追加

--- src/js/main.js
+++ src/js/main.js
@@ -21,3 +21,17 @@ import './plugins.js';
      */
   // svg4everybody();
 })(jQuery);
+
+/**
+ * JavaScriptでのレスポンシブ対応
+ */
+const matchFunction =() => {
+  if(window.matchMedia('(min-width:740px)').matches){
+    console.log('PC');
+  } else {
+    console.log('SP');
+  }
+};
+
+window.addEventListener('load', matchFunction );
+window.addEventListener('resize', matchFunction );
