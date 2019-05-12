6aca726dff7b8d5e30f44439f076f44863405a55
Ryoichi Shiohama <ryoichi.shiohama@gmail.com>
2019-05-12 14:15:55 +0900

    タスクランナーのディレクトリ設定ファイルを修正

--- tasks/config.js
+++ tasks/config.js
@@ -1,17 +1,18 @@
-const ASSET_ROOT = 'src';
-const DEST_ROOT = 'public';
+const ASSET_ROOT      = 'src';
+const DEST_ROOT       = 'public';
+const ASSET_DEST_ROOT = `${DEST_ROOT}/assets`;
 
 export const sass = {
   src: `${ASSET_ROOT}/sass/**/*.scss`,
   rootFile: `${ASSET_ROOT}/sass/_import.scss`,
   rootSrc: `${ASSET_ROOT}/sass/`,
-  dest: `${DEST_ROOT}/assets/css`
+  dest: `${ASSET_DEST_ROOT}/css`
 };
 
 export const scripts = {
   srcRoot: `${ASSET_ROOT}/js`,
   src: `${ASSET_ROOT}/js/**/*.js`,
-  dest: `${DEST_ROOT}/assets/js`,
+  dest: `${ASSET_DEST_ROOT}/js`,
   babelrc: {
     presets: [['@babel/env', { targets: '> 0.25%, not dead' }]]
   }
@@ -28,12 +29,12 @@ export const templates = {
 
 export const images = {
   src: `${ASSET_ROOT}/images/**/*.*`,
-  dest: `${DEST_ROOT}/assets/images`
+  dest: `${ASSET_DEST_ROOT}/images`
 };
 
 export const svg = {
   src: `${ASSET_ROOT}/svg/**/*.svg`,
-  dest: `${DEST_ROOT}/assets/svg`
+  dest: `${ASSET_DEST_ROOT}/svg`
 };
 
 export const isProd = process.env.NODE_ENV === 'production';
