bd05eb1b17727fdc84556f38d405ada6d4c8c121
Ryoichi Shiohama <ryoichi.shiohama@gmail.com>
2020-08-01 12:01:57 +0900

    node modulesのアップデート

--- src/js/main.js
+++ src/js/main.js
@@ -10,28 +10,28 @@ new SmoothScroll('a[href*="#"]');
 /**
  * JavaScriptでのレスポンシブ対応
  */
-const matchFunction =() => {
-  if(window.matchMedia('(min-width:740px)').matches){
+const matchFunction = () => {
+  if (window.matchMedia('(min-width:740px)').matches) {
     console.log('PC');
   } else {
     console.log('SP');
   }
 };
 
-window.addEventListener('load', matchFunction );
-window.addEventListener('resize', matchFunction );
+window.addEventListener('load', matchFunction);
+window.addEventListener('resize', matchFunction);
 
 // Web Font Loader
 window.WebFontConfig = {
   google: {
     families: ['Noto+Serif+JP:200,300,400,500,600,700,900', 'Noto+Sans+JP:100,300,400,500,700,900']
   },
-  active: function() {
+  active: function () {
     sessionStorage.fonts = true;
   }
 };
 
-(function() {
+(function () {
   const wf = document.createElement('script');
   wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
   wf.type = 'text/javascript';
